89415eb8db4069a31e7c26fb312b55dc5c2e9dcc
Lisa Wimmer <wml1293@outlook.de>
2021-01-16 22:21:44 +0100

    made overview figure

--- 2_code/0-run-visualization.R
+++ 2_code/0-run-visualization.R
@@ -59,8 +59,8 @@ dev.off()
 # EXAMPLE NEIGHBORHOOD GRAPH 3D ------------------------------------------------
 
 s_curve_connected <- plot_manifold_3d_connected(
-  make_s_curve(n_points = 300L),
-  k = 5L) 
+  make_s_curve(n_points = 1000L),
+  k = 3L) 
 
 orca(
   s_curve_connected, 
